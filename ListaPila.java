cc7510e299d2fbfbaac32d4745aed63124f8ff42
Irwing <86335744+Irwingduran@users.noreply.github.com>
2023-12-06 23:28:45 -0600

    ListaPila.java

---
+++ ListaPila.java
@@ -0,0 +1,44 @@
+public class ListaPila {
+    NodoLista head;
+    NodoLista tail;
+
+    ListaPila() {
+        head = null;
+        tail = null;
+    }
+
+    boolean estaVacia() {
+        return head == null;
+    }
+
+    void insertarHead(Object a) {
+        NodoLista nuevoNodo = new NodoLista(a);
+        if (estaVacia()) {
+            head = nuevoNodo;
+            tail = nuevoNodo;
+        } else {
+            nuevoNodo.siguiente = head;
+            head = nuevoNodo;
+        }
+    }
+
+    void insertarTail(Object a) {
+        NodoLista nuevoNodo = new NodoLista(a);
+        if (estaVacia()) {
+            head = nuevoNodo;
+            tail = nuevoNodo;
+        } else {
+            tail.siguiente = nuevoNodo;
+            tail = nuevoNodo;
+        }
+    }
+
+    void recorrerLista() {
+        NodoLista actual = head;
+        while (actual != null) {
+            System.out.print(actual.elemento + " ");
+            actual = actual.siguiente;
+        }
+        System.out.println();
+    }
+}
